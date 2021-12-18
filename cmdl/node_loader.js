c4add8b326714d8ffac78c3804f9cd4ed217d541
James Diacono <james@diacono.com.au>
2021-12-18 13:15:13 +0930

    HTTPS
    This commit imbues the Node.js padawan with the ability to fetch modules
    via HTTPS, just like Deno and the browser. It is a small change, as
    Node.js padawans already fetch modules locally via HTTP. It makes it
    possible to use services like https://esm.sh, thereby bypassing the
    node_modules directory.

--- cmdl/node_loader.js
+++ cmdl/node_loader.js
@@ -4,13 +4,16 @@
 // import modules via HTTP.
 
 import http from "http";
+import https from "https";
+
+const rx_http = /^https?:\/\//;
 
 function resolve(specifier, context, default_resolve) {
     const parent_url = context.parentURL;
-    if (specifier.startsWith("http://")) {
+    if (rx_http.test(specifier)) {
         return {url: specifier};
     }
-    if (parent_url && parent_url.startsWith("http://")) {
+    if (parent_url && rx_http.test(parent_url)) {
         return {
             url: new URL(specifier, parent_url).href
         };
@@ -19,14 +22,22 @@ function resolve(specifier, context, default_resolve) {
 }
 
 function load(url, context, default_load) {
-    if (url.startsWith("http://")) {
+    if (rx_http.test(url)) {
 
 // Load the module's source code from the network.
 
+        const http_module = (
+            url.startsWith("https:")
+            ? https
+            : http
+        );
         return new Promise(function (resolve, reject) {
-            return http.get(
+            return http_module.get(
                 url,
                 function read_response(res) {
+                    if (res.statusCode !== 200) {
+                        return reject(new Error("Failed to load module."));
+                    }
                     let source = "";
                     res.on("data", function (chunk) {
                         source += chunk;
