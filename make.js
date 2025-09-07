jamesdiacono/Replete

commit 99a460dafea3fe6cd350b369e3920fd6d4bbc5e5

Command ID lost on failure

make.js
@@ -147,7 +147,9 @@ function make_replete({
 // letters.
 
         if (!locator_href.toLowerCase().startsWith(root_href.toLowerCase())) {
-            return Promise.reject(new Error("Forbidden: " + locator));
+            return Promise.reject(new Error(
+                "Forbidden: " + locator + " is outside " + root_href
+            ));
         }
         return read(locator);
     }
@@ -245,6 +247,11 @@ function make_replete({
             on_result({
                 evaluation,
                 exception,
+                id: message.id
+            });
+        }).catch(function (error) {
+            on_result({
+                exception: error.stack,
                 id: message.id
             });
         });

run.js
@@ -82,19 +82,13 @@ function run(options) {
         });
     }
 
-    function on_error(error) {
-        on_result({
-            err: (error.stack ?? error) + "\n"
-        });
-    }
-
     start().then(function () {
         line_reader.on("line", function (line) {
-            send(JSON.parse(line)).catch(on_error);
+            send(JSON.parse(line));
         });
-    }).catch(
-        on_error
-    );
+    }).catch(function (error) {
+        on_result({err: error.stack + "\n"});
+    });
     process.on("SIGTERM", exit);
     process.on("SIGINT", exit);
     if (os.platform() !== "win32") {

repl.js
@@ -1562,8 +1562,8 @@ function make_repl(capabilities, on_start, on_eval, on_stop, specify) {
 
     function module_hash(locator, parent_locator) {
 
-// The 'module_hash' function produces a hash string for a module, or if the
-// 'locator' is not hashable. The resulting hash is tentatively cached.
+// The 'module_hash' function produces a hash string for a module, or undefined
+// if the 'locator' is not hashable. The resulting hash is tentatively cached.
 
 // The hash is dependent on:
 
@@ -1795,7 +1795,7 @@ function make_repl(capabilities, on_start, on_eval, on_stop, specify) {
                 const tree = parse_module(message.source);
                 const top_analysis = analyze_top(tree);
                 const module_analysis = analyze_module(tree);
-                const nr_dynamic = module_analysis.imports.length;
+                const dynamic_nr = module_analysis.imports.length;
                 return Promise.all(
                     all_specifiers(module_analysis).map(function (specifier) {
                         return locate(
@@ -1823,8 +1823,8 @@ function make_repl(capabilities, on_start, on_eval, on_stop, specify) {
                                 message.scope
                             );
                         },
-                        resolved_specifiers.slice(nr_dynamic),
-                        resolved_specifiers.slice(0, nr_dynamic),
+                        resolved_specifiers.slice(dynamic_nr),
+                        resolved_specifiers.slice(0, dynamic_nr),
                         top_analysis.wait
                     );
                 });

commit 0e57b9ccfd4fcd67b7335654412bfa6dca58fec1

Missing platform crash
Commit 99a460d introduced a bug whereby attempting to use a missing
platform crashed Replete.

make.js
@@ -249,11 +249,6 @@ function make_replete({
                 exception,
                 id: message.id
             });
-        }).catch(function (error) {
-            on_result({
-                exception: error.stack,
-                id: message.id
-            });
         });
     }
 

run.js
@@ -50,13 +50,7 @@ import url from "node:url";
 import make_replete from "./make.js";
 
 function run(options) {
-
-    function on_result(message) {
-        process.stdout.write(JSON.stringify(message) + "\n");
-    }
-
     options = Object.assign({}, options);
-    options.on_result = on_result;
     options.root_locator = (
         options.root_locator
         ?? url.pathToFileURL(process.cwd()).href
@@ -86,6 +80,10 @@ function run(options) {
     const line_reader = readline.createInterface({input: process.stdin});
     const {start, send, stop} = make_replete(options);
 
+    function on_result(message) {
+        process.stdout.write(JSON.stringify(message) + "\n");
+    }
+
     function exit() {
         line_reader.close();
         stop().then(function () {
@@ -95,7 +93,13 @@ function run(options) {
 
     start().then(function () {
         line_reader.on("line", function (line) {
-            send(JSON.parse(line));
+            const message = JSON.parse(line);
+            send(message).catch(function (error) {
+                on_result({
+                    exception: error.stack,
+                    id: message.id
+                });
+            });
         });
     }).catch(function (error) {
         on_result({err: error.stack + "\n"});